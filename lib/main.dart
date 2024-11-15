a51405a18d6c2ef92465ab5f82197339d44da3b0
najmul <mdnajmulislam10992@gmail.com>
2024-11-15 16:37:34 +0600

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:giga_store/home_page.dart';
+import 'package:giga_store/pages/home_page.dart';
+import 'package:giga_store/pages/login_page.dart';
 
 void main() {
   runApp(GigaStore());
 }
 
 class GigaStore extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomePage(),
-      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple, // Corrected here
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(), // Entry route
+        "/home": (context) => HomePage(), // Corrected unique name
+        //"/login": (context) => LoginPage()
+      },
     );
   }
 }
