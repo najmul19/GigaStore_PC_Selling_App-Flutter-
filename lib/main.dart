0bf876ad7370a9ad896b8b9651e54eb50a7c294b
najmul <mdnajmulislam10992@gmail.com>
2024-11-15 16:00:20 +0600

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:giga_store/home_page.dart';
 
 void main() {
   runApp(GigaStore());
 }
 
 class GigaStore extends StatelessWidget {
-  const GigaStore({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("gigaStore"),
-          ),
-        ),
-      ),
+      home: const HomePage(),
       
     );
   }
